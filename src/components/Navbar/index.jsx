fdf3c70a2383a8c100c24a7fb2fd1c2e98813fc1
robs-am <roberta.amaro89@gmail.com>
2022-10-26 02:52:40 -0300

    Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper">
+            <ul>
+                <li>Como fazer</li>
+                <li>Ofertas</li>
+                <li>Depoimentos</li>
+                <li>Vídeos</li>
+                <li>Meu Carrinho</li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar;
