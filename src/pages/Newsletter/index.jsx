e342a09cee09de0dd8e8d50916665fdf53c4a2c6
robs-am <roberta.amaro89@gmail.com>
2022-11-01 14:09:44 -0300

    add mobile first

--- src/pages/Newsletter/index.jsx
+++ src/pages/Newsletter/index.jsx
@@ -4,12 +4,13 @@ import "./Newsletter.css"
 const Newsletter = ({email, placeholder}) => {
   return (
     <section className='hero-section'>
-      <p>sua casa com as</p>
+      <p>Sua casa com as</p>
       <h1>melhores plantas</h1>
-      <p>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. 
+      <p className='p2'>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. 
         Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.
       </p>
-      <input type="email" placeholder={placeholder}>{email}</input>
+      <input type="email" id='email' placeholder={placeholder} required={true}/>{email}
+      <button className='btn-newsletter' type='submit'>Assinar Newsletter</button>
     </section>
     
   )
