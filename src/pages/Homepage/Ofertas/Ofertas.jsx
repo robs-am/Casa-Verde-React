bd6da1f2b4b8aee443d9aa626e6776ac76059297
robs-am <roberta.amaro89@gmail.com>
2022-11-26 12:42:02 -0300

    add StyleSection ofertas

--- src/pages/Homepage/Ofertas/Ofertas.jsx
+++ src/pages/Homepage/Ofertas/Ofertas.jsx
@@ -1,36 +1,38 @@
-
-
 import React from "react";
 import styled from "styled-components";
 
+const StyleSection = styled.section`
+  display: flex;
+  align-items: left;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const Styleh3 = styled.h3`
-font-weight: 400;
-font-size: 22px;
-line-height: 27px;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 27px;
 
+  text-align: left;
 
-text-align: center;
+  color: #202020;
 
-color: #202020;
-
-opacity: 0.5;
+  opacity: 0.5;
 `;
 
 const Ofertas = () => {
-
   return (
-    <main>
+    <StyleSection>
       <Styleh3>Conheça nossas</Styleh3>
       <h1>ofertas</h1>
       <section className="ofertas-wrapper">
         <div className="Ajuga">
-            <h2>Ajuga reptans</h2>
-            <p>R$ 20,00</p>
-            <p>Comprar </p>
+          <h2>Ajuga reptans</h2>
+          <p>R$ 20,00</p>
+          <p>Comprar </p>
         </div>
       </section>
-    </main>
+    </StyleSection>
   );
 };
 
